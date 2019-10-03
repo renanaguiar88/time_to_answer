c044570caf687a908205d7938188d35589dcc113
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-10-03 16:34:01 +0000

    Revert "Adicionado jquery em vendor/assets/javastripts"
    This reverts commit a0e414b1437d2ed0b9aa4494eacf1608b8a7fd86.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,9 @@ append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "storage", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "storage", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "node_modules"
+# https://github.com/capistrano/npm/issues/7
+before 'deploy:reverted', 'npm:install'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
